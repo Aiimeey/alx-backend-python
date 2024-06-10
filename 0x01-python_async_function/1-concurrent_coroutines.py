af27e83e3da51a6b91aa419cdede7d8851c23840
imane <imanezenasnii@gmail.com>
2024-06-10 14:56:22 +0100

    Add script defines an asynchronous coroutine that executes the wait_random coroutine 'n' times

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,11 +5,11 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous coroutine that executes the wait_random coroutine 'n' times
     """
     # x = []
     # for _ in range(n):
     #     x.append(wait_random(max_delay))
     x = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*x)
+    return sorted(await asyncio.gather(*x))
