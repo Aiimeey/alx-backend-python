d1c408bdecbf7a0706b0a4d138b66dce67474fe9
imane <imanezenasnii@gmail.com>
2024-06-11 11:27:03 +0100

    Add coroutine that loops 10 times asynchronously then yield a random number

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" module provides an asynchronous coroutine """
+import asyncio
+import random
+from typing import List
+
+
+async def async_generator() -> List[float]:
+    """ coroutine that loops 10 times asynchronously wait 1 second,
+        then yield a random number"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
